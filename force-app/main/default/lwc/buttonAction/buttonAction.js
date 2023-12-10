1019afd1c3b350d52cf0b217216f0cea60d576a0
Camila Bentancor <belenbentancor23@gmail.com>
2023-12-09 22:57:50 -0300

    Data sabado

--- force-app/main/default/lwc/buttonAction/buttonAction.js
+++ force-app/main/default/lwc/buttonAction/buttonAction.js
@@ -1,9 +1,25 @@
 import { LightningElement } from 'lwc';
+import ucl_NightlyProcessor from '@salesforce/apex/ucl_NightlyProcessor.herokuCall';
+import { ShowToastEvent } from 'lightning/platformShowToastEvent';
 
-export default class ButtonBasic extends LightningElement {
-    clickedButtonLabel;
+export default class ButtonAction extends LightningElement {
 
-    handleClick(event) {
-        this.clickedButtonLabel = event.target.label;
+    handleClick() {
+        ucl_NightlyProcessor()
+            .then(result => {
+                this.showToast('Éxito', 'Datos importados exitosamente desde Heroku', 'success');
+            })
+            .catch(error => {
+                this.showToast('Error', `Error al importar datos: ${error.body.message}`, 'error');
+            });
+    }
+
+    showToast(title, message, variant) {
+        const toastEvent = new ShowToastEvent({
+            title: title,
+            message: message,
+            variant: variant,
+        });
+        this.dispatchEvent(toastEvent);
     }
 }
